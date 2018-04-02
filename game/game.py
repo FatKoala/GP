9f81122d7f2d7324f05a06cc727ceef731cf86f3
Dejan Nedic <dejan.nedic@ebs.msft>
2018-04-03 00:37:47 +0200

    added game package

---
+++ game/game.py
@@ -0,0 +1,9 @@
+class Game:
+    def __init__(self):
+        print("New game has been created!")
+
+    def newGame(self):
+        print("Starting new game!")
+
+    def close(self):
+        print("Game has been closed!")
